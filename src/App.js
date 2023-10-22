031eb5ce57e1513328f9e84522380c75ce460c67
akashchowdarykodali <akashkodali6@gmail.com>
2023-10-22 16:00:52 +0530

    index and home js files update and structured

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HomePage from "./pages/home";
+import {HashRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import ScrollToTop from "./pages/scrolltoTop";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <ScrollToTop/>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
