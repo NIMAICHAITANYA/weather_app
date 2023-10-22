89053442ac1d1475981b68ab3ae783033a0b4d71
akashchowdarykodali <akashkodali6@gmail.com>
2023-10-22 16:32:54 +0530

    app.js update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import {HashRouter as Router,
   Route,
   Routes
 } from "react-router-dom";
-import ScrollToTop from "./pages/scrolltoTop";
 
 function App() {
   return (
     <>
       <Router>
-        <ScrollToTop/>
         <Routes>
           <Route exact path="/" element={<HomePage />} />
         </Routes>
